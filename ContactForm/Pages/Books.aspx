a14a4d328c65a862ef39fd45e21514ba82d9be28
olegstabr <olegstabr@gmail.com>
2017-10-20 19:10:40 +0700

    Add books page

---
+++ ContactForm/Pages/Books.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Menu.Master" AutoEventWireup="true" CodeBehind="Books.aspx.cs" Inherits="ContactForm.Pages.Books" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div align="center">
+        <h1> Данные из таблицы "Книги"</h1>
+        <asp:GridView runat="server" ID="BookGridView" BackColor="White"  AllowPaging="True">          
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ ContactForm/Pages/Books.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Data;
+using System.Web.UI.WebControls;
+using Npgsql;
+
+namespace ContactForm.Pages
+{
+    public partial class Books : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BookGridView.RowDataBound +=  OnBookGridViewRowDataBound;
+            BookGridView.DataSource = GetBooks();
+            BookGridView.DataBind();
+        }
+
+        DataTable GetBooks()
+        {
+            DataTable table = null;
+            string connString = "server=localhost;user id=postgres;password=1234;database=POIS";
+            string query = "SELECT * FROM BOOK";
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(connString))
+            {
+                try
+                {
+                    using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+                    {
+
+                        NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(command);
+                        table = new DataTable();
+                        adapter.Fill(table);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Response.Write(e);
+                }
+            }
+
+            return table;
+        }
+
+        void OnBookGridViewRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType != DataControlRowType.Header) { return; }
+
+            var rowCells = e.Row.Cells;
+            string header = string.Empty;
+            for (int i = 0; i < rowCells.Count; i++)
+            {
+                switch (rowCells[i].Text)
+                {
+                    case "id":
+                        header = "№";
+                        break;
+                    case "name":
+                        header = "Навзание";
+                        break;
+                    case "author":
+                        header = "Автор";
+                        break;
+                    case "year":
+                        header = "Год издания";
+                        break;
+                    case "publish_house":
+                        header = "Издательство";
+                        break;
+                }
+                rowCells[i].Text = header;
+            }
+        }
+    }
+}
