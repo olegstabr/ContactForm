6cffea7fb2273e28e956c462d204d058f38d10b4
olegstabr <olegstabr@gmail.com>
2017-10-21 18:39:28 +0700

    Add, delete, update books from web page

--- ContactForm/Pages/Books.aspx.cs
+++ ContactForm/Pages/Books.aspx.cs
@@ -1,44 +1,33 @@
 ﻿using System;
 using System.Data;
 using System.Web.UI.WebControls;
+using ContactForm.Entity;
+using ContactForm.Service;
 using Npgsql;
 
 namespace ContactForm.Pages
 {
     public partial class Books : System.Web.UI.Page
     {
+        readonly DataBaseService dataBaseService = new DataBaseService();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            AddBookButton.Click += OnAddBookButtonClick;
+
             BookGridView.RowDataBound +=  OnBookGridViewRowDataBound;
-            BookGridView.DataSource = GetBooks();
-            BookGridView.DataBind();
+            BookGridView.RowEditing += OnBookGridViewRowEditing;
+            BookGridView.RowDeleting += OnBookGridViewRowDeleting;
+            BookGridView.RowUpdating += OnBookGridViewRowUpdating;
+            BookGridView.RowCancelingEdit += OnBookGridViewRowCancelingEdit;
+            BookGridView.DataSource = dataBaseService.GetBooks();
+            if (!IsPostBack) { BookGridView.DataBind(); }
         }
 
-        DataTable GetBooks()
+        void UpdateData()
         {
-            DataTable table = null;
-            string connString = "server=localhost;user id=postgres;password=1234;database=POIS";
-            string query = "SELECT * FROM BOOK";
-
-            using (NpgsqlConnection connection = new NpgsqlConnection(connString))
-            {
-                try
-                {
-                    using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
-                    {
-
-                        NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(command);
-                        table = new DataTable();
-                        adapter.Fill(table);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Response.Write(e);
-                }
-            }
-
-            return table;
+            BookGridView.DataSource = dataBaseService.GetBooks();
+            BookGridView.DataBind();
         }
 
         void OnBookGridViewRowDataBound(object sender, GridViewRowEventArgs e)
@@ -46,8 +35,8 @@ namespace ContactForm.Pages
             if (e.Row.RowType != DataControlRowType.Header) { return; }
 
             var rowCells = e.Row.Cells;
-            string header = string.Empty;
-            for (int i = 0; i < rowCells.Count; i++)
+            var header = string.Empty;
+            for (var i = 0; i < rowCells.Count; i++)
             {
                 switch (rowCells[i].Text)
                 {
@@ -69,6 +58,60 @@ namespace ContactForm.Pages
                 }
                 rowCells[i].Text = header;
             }
+        }
+        
+        void OnAddBookButtonClick(object sender, EventArgs e)
+        {
+            var name = NameTextBox.Text;
+            var author = AuthorTextBox.Text;
+            var year = Convert.ToInt32(YearTextBox.Text);
+            var publishHouse = PublishHouseTextBox.Text;
+            var bookEntity = new BookEntity(name, author, year, publishHouse);
+
+            NameTextBox.Text = string.Empty;
+            AuthorTextBox.Text = string.Empty;
+            YearTextBox.Text = string.Empty;
+            PublishHouseTextBox.Text = string.Empty;
+
+            dataBaseService.AddBook(bookEntity);
+            UpdateData();
+        }
+
+        void OnBookGridViewRowEditing(object sender, GridViewEditEventArgs e)
+        {
+            BookGridView.EditIndex = e.NewEditIndex;
+            UpdateData();
+        }
+
+        void OnBookGridViewRowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            var values = e.Values;
+            int id = Convert.ToInt32(values["id"]);
+            dataBaseService.DeleteBook(id);
+            UpdateData();
+        }
+
+        void OnBookGridViewRowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            if (BookGridView.Rows.Count <= e.RowIndex) { return; }
+
+            var values = e.NewValues;
+            var id = Convert.ToInt32(values["id"]);
+            var name = values["name"].ToString();
+            var author = values["author"].ToString();
+            var year = Convert.ToInt32(values["year"]);
+            var publishHouse = values["publish_house"].ToString();
+            var bookEntity = new BookEntity(name, author, year, publishHouse) {ID = id};
+
+            dataBaseService.UpdateBook(bookEntity);
+            
+            BookGridView.EditIndex = -1;
+            UpdateData();
+        }
+
+        void OnBookGridViewRowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            BookGridView.EditIndex = -1;
         }
     }
 }
--- ContactForm/Pages/Books.aspx.designer.cs
+++ ContactForm/Pages/Books.aspx.designer.cs
@@ -20,5 +20,50 @@ namespace ContactForm.Pages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView BookGridView;
+        
+        /// <summary>
+        /// NameTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox NameTextBox;
+        
+        /// <summary>
+        /// AuthorTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox AuthorTextBox;
+        
+        /// <summary>
+        /// YearTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox YearTextBox;
+        
+        /// <summary>
+        /// PublishHouseTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox PublishHouseTextBox;
+        
+        /// <summary>
+        /// AddBookButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button AddBookButton;
     }
 }
