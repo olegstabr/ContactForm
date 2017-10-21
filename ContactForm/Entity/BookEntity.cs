65f3d6c876c6b6a7faacef99426f72a1525615f7
olegstabr <olegstabr@gmail.com>
2017-10-21 18:38:39 +0700

    Add BookEntity

---
+++ ContactForm/Entity/BookEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace ContactForm.Entity
+{
+    public sealed class BookEntity
+    {
+        public int ID;
+        public string Name; 
+        public string Author;
+        public int Year;
+        public string PublishHouse;
+
+        public BookEntity() { }
+
+        public BookEntity(string name, string author, int year, string publishHouse)
+        {
+            Name = name;
+            Author = author;
+            Year = year;
+            PublishHouse = publishHouse;
+        }
+    }
+}
